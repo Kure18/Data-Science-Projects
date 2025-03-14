9827feb1d5a152df10cae815c83751898d298cb0
Kure18 <11gsolo@gmail.com>
2025-03-14 14:02:25 +0100

    playersDML2

--- players.py
+++ players.py
@@ -34,3 +34,50 @@ print(players["ict_index_rank"].cummax())
 print(players["ict_index_rank"].cumprod())
 
 print(players["minutes_value"].agg(["mean","max"]))
+
+print(players.head(20))
+
+print(players.drop_duplicates(subset="name"))
+
+print(players["position"].value_counts(sort=True))
+
+print(players["position"].value_counts(normalize=True))
+
+print(players.groupby("position")["now_cost_rank_type"].mean())
+
+print(players.groupby("team")["minutes"].agg(["min","max","mean"]))
+
+print(players.groupby("name")["now_cost_rank"].mean())
+
+print(players.groupby(["name","team"])[["minutes"]].mean())
+
+print(players.pivot_table(index="position",values="minutes"))
+
+print(players.pivot_table(index="team",values="total_points",columns="now_cost_rank_type",fill_value=0,margins=True))
+
+import numpy as np
+print(players.pivot_table(index="team",values="minutes",aggfunc=[np.mean,np.min,np.max]))
+
+print(players.set_index("minutes"))
+
+june = players.set_index("minutes")
+print(june)
+
+print(june.reset_index())
+
+print(june.reset_index(drop=True))
+
+july = players.set_index("team")
+
+print(july)
+
+print(july.loc["Liverpool"])
+
+print(july.loc["Wolves"])
+
+august = players.set_index(["team","name"])
+print(august)
+
+print(august.loc[["Liverpool","Brentford"]])
+
+print(august.sort_index())
